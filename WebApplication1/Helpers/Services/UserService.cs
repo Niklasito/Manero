f9b8ab6b9d916ef470a4e3d77c0bd77180d9da27
jocke94 <joakim94larsson@gmail.com>
2023-10-13 16:08:51 +0200

    Claims

---
+++ WebApplication1/Helpers/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using Manero.Models.Contexts;
+using Manero.Models.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Manero.Helpers.Services;
+
+public class UserService
+{
+    private readonly UserManager<ManeroUser> _userManager;
+    private readonly DataContext _context;
+
+    public UserService(UserManager<ManeroUser> userManager, DataContext context)
+    {
+        _userManager = userManager;
+        _context = context;
+    }
+
+    public async Task<ManeroUser> GetUserInfoAsync(string Email)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(x => x.Email == Email);
+        return user;
+    }
+}
