f2533f68e0f5a4c9d6e46c0be16c3156217993ba
Peter Rydden <peter.ryden@utb.ecutbildning.se>
2023-10-25 22:17:24 +0200

    RememberMe Test

---
+++ Manero.Test/Tests/Peter/javascript/PasswordValidationTest.cs
@@ -0,0 +1,82 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace Manero.Test.Tests.Peter.javascript;
+
+public class PasswordValidationTests : IClassFixture<ChromeDriverFixture>
+{
+    private readonly IWebDriver _driver;
+
+    public PasswordValidationTests(ChromeDriverFixture fixture)
+    {
+        _driver = fixture.Driver;
+    }
+
+    [Fact]
+    public void TestInvalidPassword()
+    {
+        _driver.Navigate().GoToUrl("https://localhost:7003/account/register");
+
+        IWebElement passwordInput = _driver.FindElement(By.CssSelector("[data-val='true'][type='password'][name='Password']"));
+        passwordInput.SendKeys("weakpassword");
+
+        System.Threading.Thread.Sleep(3000);
+
+        IWebElement validationMessage = _driver.FindElement(By.CssSelector("[data-valmsg-for='Password']"));
+        Assert.Equal("Password is invalid", validationMessage.Text);
+    }
+
+    [Fact]
+    public void TestValidPassword()
+    {
+        _driver.Navigate().GoToUrl("https://localhost:7003/account/register");
+
+        IWebElement passwordInput = _driver.FindElement(By.CssSelector("[data-val='true'][type='password'][name='Password']"));
+        IWebElement confirmPasswordInput = _driver.FindElement(By.CssSelector("[data-val='true'][type='password'][name='ConfirmPassword']"));
+
+        passwordInput.SendKeys("Password123!");
+
+        System.Threading.Thread.Sleep(3000);
+
+        IWebElement validationMessage = _driver.FindElement(By.CssSelector("[data-valmsg-for='ConfirmPassword']"));
+        Assert.True(string.IsNullOrEmpty(validationMessage.Text) || !validationMessage.Displayed);
+    }
+
+    [Fact]
+    public void TestMatchingPasswords()
+    {
+        _driver.Navigate().GoToUrl("https://localhost:7003/account/register");
+
+        IWebElement passwordInput = _driver.FindElement(By.CssSelector("[data-val='true'][type='password'][name='Password']"));
+        IWebElement confirmPasswordInput = _driver.FindElement(By.CssSelector("[data-val='true'][type='password'][name='ConfirmPassword']"));
+
+        passwordInput.SendKeys("Password123!");
+        confirmPasswordInput.SendKeys("Password123!");
+
+        System.Threading.Thread.Sleep(3000);
+
+        IWebElement validationMessage = _driver.FindElement(By.CssSelector("[data-valmsg-for='ConfirmPassword']"));
+        Assert.True(string.IsNullOrEmpty(validationMessage.Text) || !validationMessage.Displayed);
+    }
+
+    [Fact]
+    public void TestNonMatchingPasswords()
+    {
+        _driver.Navigate().GoToUrl("https://localhost:7003/account/register");
+
+        IWebElement passwordInput = _driver.FindElement(By.CssSelector("[data-val='true'][type='password'][name='Password']"));
+        IWebElement confirmPasswordInput = _driver.FindElement(By.CssSelector("[data-val='true'][type='password'][name='ConfirmPassword']"));
+
+        passwordInput.SendKeys("Password123!");
+        confirmPasswordInput.SendKeys("Password234!");
+
+        System.Threading.Thread.Sleep(3000);
+
+        IWebElement validationMessage = _driver.FindElement(By.CssSelector("[data-valmsg-for='ConfirmPassword']"));
+        Assert.Equal("Passwords do not match", validationMessage.Text);
+    }
+
+    
+}
+
---
+++ Manero.Test/Tests/Peter/javascript/RememberMeTest.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Manero.Test.Tests.Peter.javascript;
+
+
+public class RememberMeTest : IClassFixture<ChromeDriverFixture>
+{
+    private readonly IWebDriver _driver;
+
+    public RememberMeTest(ChromeDriverFixture fixture)
+    {
+        _driver = fixture.Driver;
+    }
+
+    [Fact]
+    public void TestRememberMeFunctionality()
+    {
+        _driver.Navigate().GoToUrl("https://localhost:7003/account");
+
+        IWebElement rememberMeCheckbox = _driver.FindElement(By.CssSelector(".sign-in-input-checkbox"));
+        IWebElement emailInput = _driver.FindElement(By.CssSelector("[data-val='true'][type='email'][name='Email']"));
+        IWebElement passwordInput = _driver.FindElement(By.CssSelector("[data-val='true'][type='password'][name='Password']"));
+
+        emailInput.SendKeys("test@example.com");
+        passwordInput.SendKeys("Password123!");
+        rememberMeCheckbox.Click();
+
+        System.Threading.Thread.Sleep(2000);
+
+        IWebElement signInButton = _driver.FindElement(By.CssSelector(".sign-in-input[type='submit']"));
+
+        emailInput.Clear();
+        System.Threading.Thread.Sleep(1000);
+        passwordInput.Clear();
+        System.Threading.Thread.Sleep(1000);
+        rememberMeCheckbox.Click();
+
+        System.Threading.Thread.Sleep(3000);
+
+        emailInput.SendKeys("test@example.com");
+        System.Threading.Thread.Sleep(1000);
+        passwordInput.SendKeys("Password123!");
+        System.Threading.Thread.Sleep(1000);
+        rememberMeCheckbox.Click();
+
+        System.Threading.Thread.Sleep(3000);
+
+        signInButton.Click();
+    }
+}
