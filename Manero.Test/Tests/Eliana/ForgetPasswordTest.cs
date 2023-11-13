16cad57710e4c060a0de39d0b27087897160f41d
Peter Rydden <peter.ryden@utb.ecutbildning.se>
2023-11-13 20:38:15 +0100

    test editProfile

--- Manero.Test/Tests/Eliana/ForgetPasswordTest.cs
+++ Manero.Test/Tests/Eliana/ForgetPasswordTest.cs
@@ -19,23 +19,28 @@ public class ForegetPassword
 
     public void LogIn_User_To_Forget_Password()
     {
-        //Arrange
-
-        var _signInManagerMock = new Mock<SignInManager<ManeroUser>>(
-                new Mock<UserManager<ManeroUser>>(
-                    new Mock<IUserStore<ManeroUser>>().Object,
-                    null!, null!, null!, null!, null!, null!, null!, null!
-                ).Object,
-                new HttpContextAccessor(),
-                new Mock<IUserClaimsPrincipalFactory<ManeroUser>>().Object,
-                null!, null!, null!, null!
-            );
+        // Arrange
+        var authMock = new Mock<InterfaceAuthenticationService>();
+        var editMock = new Mock<InterfaceEdietProfileService>();
+        var userManagerMock = new Mock<UserManager<ManeroUser>>(
+        new Mock<IUserStore<ManeroUser>>().Object,
+        null!, null!, null!, null!, null!, null!, null!, null!
+        );
+        var signInManagerMock = new Mock<SignInManager<ManeroUser>>(
+            new Mock<UserManager<ManeroUser>>(
+                new Mock<IUserStore<ManeroUser>>().Object,
+                null!, null!, null!, null!, null!, null!, null!, null!
+            ).Object,
+            new HttpContextAccessor(),
+            new Mock<IUserClaimsPrincipalFactory<ManeroUser>>().Object,
+            null!, null!, null!, null!
+        );
 
         var _authServiceMock = new Mock<AuthenticationService>();
 
-        _signInManagerMock.Setup(x => x.IsSignedIn(It.IsAny<ClaimsPrincipal>())).Returns(true);
+        signInManagerMock.Setup(x => x.IsSignedIn(It.IsAny<ClaimsPrincipal>())).Returns(true);
 
-        var controller = new AccountController(_authServiceMock.Object, _signInManagerMock.Object);
+        var controller = new AccountController(authMock.Object, signInManagerMock.Object, editMock.Object, userManagerMock.Object);
 
         //Act
         var result = controller.ForgetPassword() as ViewResult;
